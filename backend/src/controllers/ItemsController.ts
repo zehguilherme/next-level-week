879fd58e5657d184224399981dba0eaf5576afbc
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-04 12:06:43 -0300

    Add ItemsController and PointsController

---
+++ backend/src/controllers/ItemsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import knex from '../database/connection'; //conexão com o banco
+
+class ItemsController {
+  // Listar todos os itens
+  async index(request: Request, response: Response) {
+    const items = await knex('items').select('*');
+
+    //As informações do banco não estão inicialmente da melhor maneira para que possam ser retornadas para o front-end, cliente
+    //O ato de transformar esses dados/informações para um novo formato que será mais acessível para quem está requisitando essas informações é chamado de serialização
+
+    //map: percorre todos os itens
+    const serializedItems = items.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        image_url: `http://localhost:3333/uploads/${item.image}`,
+      };
+    });
+
+    return response.json(serializedItems);
+  }
+}
+
+export default ItemsController;

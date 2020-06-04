5d1d20d9fb374f4132971474c74ded6b92830606
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-04 12:08:28 -0300

    Add all tables migrations

---
+++ backend/src/database/migrations/02_create_point_items.ts
@@ -0,0 +1,25 @@
+import Knex from 'knex'; //Letra maiúscula: tipo da variável
+
+// Realizar alterações no banco
+// Criar tabela
+export async function up(knex: Knex) {
+  return knex.schema.createTable('point_items', (table) => {
+    table
+      .integer('point_id')
+      .notNullable()
+      .references('id')
+      .inTable('points');
+
+    table
+      .integer('item_id')
+      .notNullable()
+      .references('id')
+      .inTable('items');
+  });
+}
+
+// Desfazer alterações no banco - o contrário feito no up()
+// Deletar tabela
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('point_items');
+}

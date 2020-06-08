16a021bea5f812b18b929905b07aa2228676fd9f
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-08 12:30:19 -0300

    Add App.tsx

---
+++ mobile/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { AppLoading } from 'expo';
+import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+
+import Routes from './src/routes';
+
+export default function App() {
+  // Quando a fonte terminou de ser carregada
+  const [fontsLoaded] = useFonts([
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold,
+  ]);
+
+  // Enquanto as fontes ainda não foram carregadas
+  if (!fontsLoaded) {
+    return <AppLoading />; //sinal de carregamento
+  }
+
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </>
+  );
+}

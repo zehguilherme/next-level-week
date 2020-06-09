4481db5322b9ff28ff42d3cb9675ddeb94b5da1e
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-09 13:52:37 -0300

    Add Dropzone component

---
+++ frontend/src/components/Dropzone/index.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+
+import './styles.css';
+
+interface Props {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Props> = ({ onFileUploaded }) => {
+  const [selectFileUrl, setSelectFileUrl] = useState(''); //guarda o previw da imagem que foi feito o upload
+
+  // acceptedFiles - recebe os arquivos
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0]; //recebe somente um arquivo
+
+      const fileUrl = URL.createObjectURL(file);
+
+      setSelectFileUrl(fileUrl);
+      onFileUploaded(file);
+    },
+    [onFileUploaded]
+  );
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+  });
+
+  return (
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept="image/*" />
+
+      {/* Caso tenha a URL da imagem mostra ela no preview */}
+      {/* Se não, mostra a mensagem para colocar um arquivo */}
+      {selectFileUrl ? (
+        <img src={selectFileUrl} alt="Point thumbnail" />
+      ) : (
+        <p>
+          <FiUpload />
+          Imagem do estabelecimento
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default Dropzone;

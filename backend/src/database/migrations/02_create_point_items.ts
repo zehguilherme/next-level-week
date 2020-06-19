31d766f9b81b9dc4e5d6aed75657047397f806c9
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-19 13:46:12 -0300

    Add eslint disable; update comments and createTable items

--- backend/src/database/migrations/02_create_point_items.ts
+++ backend/src/database/migrations/02_create_point_items.ts
@@ -1,25 +1,23 @@
-import Knex from 'knex'; //Letra maiúscula: tipo da variável
+/* eslint-disable no-unused-vars */
+import Knex from 'knex'; // Letra maiúscula: tipo da variável
 
-// Realizar alterações no banco
-// Criar tabela
+/*
+ * Realizar alterações no banco
+ * Criar tabela
+ */
 export async function up(knex: Knex) {
   return knex.schema.createTable('point_items', (table) => {
-    table
-      .integer('point_id')
-      .notNullable()
-      .references('id')
-      .inTable('points');
+    table.increments('id').primary();
+    table.integer('point_id').notNullable().references('id').inTable('points');
 
-    table
-      .integer('item_id')
-      .notNullable()
-      .references('id')
-      .inTable('items');
+    table.integer('item_id').notNullable().references('id').inTable('items');
   });
 }
 
-// Desfazer alterações no banco - o contrário feito no up()
-// Deletar tabela
+/*
+ * Desfazer alterações no banco - o contrário feito no up()
+ * Deletar tabela
+ */
 export async function down(knex: Knex) {
   return knex.schema.dropTable('point_items');
 }

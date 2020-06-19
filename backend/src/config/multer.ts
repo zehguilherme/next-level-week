5c39f3b3cc97388f66e4898913a932d1da3bdf68
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2020-06-19 13:51:36 -0300

    Update comments

--- backend/src/config/multer.ts
+++ backend/src/config/multer.ts
@@ -4,9 +4,9 @@ import crypto from 'crypto';
 
 export default {
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'), //onde serão guardados os arquivos que forem enviados pelo usuário
+    destination: path.resolve(__dirname, '..', '..', 'uploads'), // onde serão guardados os arquivos que forem enviados pelo usuário
     filename: (request, file, callback) => {
-      const hash = crypto.randomBytes(6).toString('hex'); //Gerar 6 bytes de caracteres aleatórios convertidos para hexadecimal
+      const hash = crypto.randomBytes(6).toString('hex'); // Gerar 6 bytes de caracteres aleatórios convertidos para hexadecimal
 
       const fileName = `${hash}-${file.originalname}`;
 
